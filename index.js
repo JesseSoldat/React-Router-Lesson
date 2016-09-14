ec4b873833a64709c9a98dd13a9446bc95116460
jesse soldat <jsoldat@hotmail.com>
2016-09-14 11:22:25 +0700

    about and repos page routing works

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import { render } from 'react-dom'
 import firebase from 'firebase'
-import { Router, Rout, hashHistory, browserHistory, Link, IndexRoute, IndexLink } from 'react-router'
+import { Router, Route, hashHistory, browserHistory, Link, IndexRoute, IndexLink } from 'react-router'
+
+import config from './firebaseConfig'
+
+firebase.initializeApp(config);
+
 
 import App from './modules/App'
+import About from './modules/About'
+import Repos from './modules/Repos'
 
 
 
-render(<App/>, document.getElementById('app'))
+render(
+	<Router history={hashHistory}>
+		<Route path="/" component={App}>
+			<Route path="/about" component={About}/>
+			<Route path="/repos" component={Repos}/>
+		</Route>
+	</Router>
+
+	, document.getElementById('app'))
--- modules/App.js
+++ modules/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router'
+import NavLink from './NavLink'
+
 
 export default React.createClass({
   render() {
-    return <div>Hello, React Router!</div>
+    return (
+    	<div>
+    		<ul>
+    			<li><NavLink to="/about">About</NavLink></li>
+    			<li><NavLink to="/repos">Repos</NavLink></li>
+    		</ul>
+    		<h2>{this.props.children}</h2>
+    	</div>
+
+
+    	)
   }
 })
